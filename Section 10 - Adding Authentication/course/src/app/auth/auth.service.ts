75be9f3ac6f86a749cf8f1192c7b338dbf321f32
Patrick Labes <patrick.labes@gmail.com>
2019-08-14 21:38:22 +1000

    started work on adding auth for the app

---
+++ Section 10 - Adding Authentication/course/src/app/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: "root" })
+export class AuthService {
+
+    signUp(email: string, password: string) {
+
+    }
+
+    login(email: string, password: string) {
+
+    }
+}
